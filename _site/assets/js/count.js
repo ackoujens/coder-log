b79457e4f0ac7bf9925e422ad5af8bd11fb98427
Ackou Jens <ackoujens@gmail.com>
2016-10-06 23:04:29 +0200

    Seems to work without count.js - Waiting for confirmation

--- _site/assets/js/count.js
+++ _site/assets/js/count.js
@@ -1,13 +1,11 @@
-<script type="text/javascript">
-      /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-      var disqus_shortname = 'perfectlyrandom'; // required: replace example with your forum shortname
-      // var disqus_developer = 1; // Comment out when the site is live
+/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+var disqus_shortname = 'ackoujens-github-io-coder-log'; // required: replace example with your forum shortname
+// var disqus_developer = 1; // Comment out when the site is live
 
-      /* * * DON'T EDIT BELOW THIS LINE * * */
-      (function () {
-        var s = document.createElement('script'); s.async = true;
-        s.type = 'text/javascript';
-        s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
-        (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-      }());
-    </script>
+/* * * DON'T EDIT BELOW THIS LINE * * */
+(function () {
+  var s = document.createElement('script'); s.async = true;
+  s.type = 'text/javascript';
+  s.src = 'http://' + disqus_shortname + '.disqus.com/count.js';
+  (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
+}());
